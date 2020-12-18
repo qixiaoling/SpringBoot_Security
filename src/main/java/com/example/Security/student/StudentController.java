e2b675487353713294e526a56e5ad08fee80bef5
qixiaoling <x.qi@novi-education.nl>
2020-12-18 13:20:56 +0100

    initial commit

---
+++ src/main/java/com/example/Security/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.Security.student;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "James Bond"),
+            new Student(3, "James Bond")
+    );
+
+
+
+    @GetMapping("/{studentId}")
+    public Student getStudent(@PathVariable ("studentId") Integer studentId){
+        return STUDENTS.stream().filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(()-> new IllegalStateException("Student"+studentId+"does not exists"));
+
+    }
+
+}
